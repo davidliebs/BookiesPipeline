67a072baeb0362a41c50571025545e515483da83
David Lieberman <david@lieberman.co.uk>
2022-04-29 22:17:17 +0100

    Can now map match names from different bookies

--- transformation/reciever.py
+++ transformation/reciever.py
@@ -1,5 +1,6 @@
 import pika, sys, os, json
 import transform_bookie_data
+import map_teams_to_match
 
 def main():
 	connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
@@ -9,8 +10,12 @@ def main():
 
 	def callback(ch, method, properties, body):
 		data = json.loads(body.decode())
+
 		TransformData = transform_bookie_data.TransformBookieData(data)
 		bookie, odds_timestamp, match_title, odds = TransformData.Run()
+
+		MapOddsToMatch = map_teams_to_match.MapOddsToMatch([bookie, odds_timestamp, match_title, odds])
+		MapOddsToMatch.Run()
 
 	channel.basic_consume(queue='odds', on_message_callback=callback, auto_ack=True)
 
